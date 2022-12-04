92374a072c9d3ccd71793cce2f0bff6e81b866a7
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-12-04 21:24:30 +0400

    subscription section

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
+import {initTabs} from './modules/tabs/init-tabs';
 
 // ---------------------------------
 
@@ -17,6 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     initModals();
+    initTabs();
   });
 });
 
